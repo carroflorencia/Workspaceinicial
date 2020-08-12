2efe3e81bc0162b4a311e85ec29d709cf715a009
carroflorencia <carroflorencia@gmail.com>
2020-08-12 10:16:13 -0300

    idchange

--- js/products.js
+++ js/products.js
@@ -1,6 +1,74 @@
+var currentProductsArray = [];
+var currentSortCriteria = undefined;
+const ORDER_ASC_BY_NAME = "AZ";
+var minCount = undefined;
+var maxCount = undefined;
+
+function showProductsList(){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < currentProductsArray.length; i++){
+        let product = currentProductsArray[i];
+
+        if (((minCount == undefined) || (minCount != undefined && parseInt(product.productCount) >= minCount)) &&
+            ((maxCount == undefined) || (maxCount != undefined && parseInt(product.productCount) <= maxCount))){
+
+            htmlContentToAppend += `
+            <a href="product-info.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ product.name +`</h4>
+                            <small class="text-muted">` + product.soldCount + ` artículos</small>
+                        </div>
+                        <p class="mb-1">` + product.description + `</p>
+                    </div>
+                </div>
+            </a>
+            `
+        }
+
+        document.getElementById("productos").innerHTML = htmlContentToAppend;
+    }
+}
+
+function sortAndShowCategories(sortCriteria, ProductsArray){
+    currentSortCriteria = sortCriteria;
+
+    if(ProductsArray != undefined){
+        currentProductsArray = ProductsArray;
+    }
+
+    currentProductsArray = sortCategories(currentSortCriteria, currentProductsArray);
+
+    //Muestro las categorías ordenadas
+    showProductsList();
+}
+
+function sortCategories(criteria, array){
+    let result = [];
+    if (criteria === ORDER_ASC_BY_NAME)
+    {
+        result = array.sort(function(a, b) {
+            if ( a.name < b.name ){ return -1; }
+            if ( a.name > b.name ){ return 1; }
+            return 0;
+        });
+    }
+
+    return result;
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            sortAndShowCategories(ORDER_ASC_BY_NAME, resultObj.data);
+        }
+    });
 });
