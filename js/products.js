7e8f3b13008e42e6f53b9b534eefb66b043e7e82
carroflorencia <carroflorencia@gmail.com>
2020-08-19 22:28:18 -0300

    Entrga2, listas ordenadas

--- js/products.js
+++ js/products.js
@@ -1,6 +1,36 @@
 var currentProductsArray = [];
 var currentSortCriteria = undefined;
-const ORDER_ASC_BY_NAME = "AZ";
+const ORDER_ASC_BY_COST = "Asc";
+const ORDER_DESC_BY_COST = "Desc";
+const ORDER_BY_SOLD_COUNT = "Cant";
+
+function sortProdcuts(criteria, array){
+    let result = [];
+    if (criteria === ORDER_ASC_BY_COST)
+    {
+        result = array.sort(function(a, b) {
+            if ( a.cost < b.cost ){ return -1; }
+            if ( a.cost > b.cost ){ return 1; }
+            return 0;
+        });
+    }else if (criteria === ORDER_DESC_BY_COST){
+        result = array.sort(function(a, b) {
+            if ( a.cost > b.cost ){ return -1; }
+            if ( a.cost < b.cost ){ return 1; }
+            return 0;
+        });
+    }else if (criteria === ORDER_BY_SOLD_COUNT){
+        result = array.sort(function(a, b) {
+            let aCount = a.soldCount;
+            let bCount = b.soldCount;
+
+            if ( aCount > bCount ){ return -1; }
+            if ( aCount < bCount ){ return 1; }
+            return 0;
+        });
+    }
+    return result;
+};
 
 function showProductsList(){
 
@@ -17,7 +47,7 @@ function showProductsList(){
                     </div>
                     <div class="col">
                         <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">`+ product.name +`</h4>
+                            <h4 class="mb-1">`+ product.name + ": " + product.cost + " " + product.currency + `</h4>
                             <small class="text-muted">` + product.soldCount + ` artículos</small>
                         </div>
                         <p class="mb-1">` + product.description + `</p>
@@ -30,32 +60,20 @@ function showProductsList(){
     }
 }
 
-function sortAndShowCategories(sortCriteria, ProductsArray){
+function sortAndShowProducts(sortCriteria, ProductsArray){
     currentSortCriteria = sortCriteria;
 
     if(ProductsArray != undefined){
         currentProductsArray = ProductsArray;
     }
 
-    currentProductsArray = sortCategories(currentSortCriteria, currentProductsArray);
+    currentProductsArray = sortProdcuts(currentSortCriteria, currentProductsArray);
 
     //Muestro las categorías ordenadas
     showProductsList();
 }
 
-function sortCategories(criteria, array){
-    let result = [];
-    if (criteria === ORDER_ASC_BY_NAME)
-    {
-        result = array.sort(function(a, b) {
-            if ( a.name < b.name ){ return -1; }
-            if ( a.name > b.name ){ return 1; }
-            return 0;
-        });
-    }
 
-    return result;
-}
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
@@ -63,7 +81,19 @@ function sortCategories(criteria, array){
 document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(PRODUCTS_URL).then(function(resultObj){
         if (resultObj.status === "ok"){
-            sortAndShowCategories(ORDER_ASC_BY_NAME, resultObj.data);
+            sortAndShowProducts(ORDER_ASC_BY_COST, resultObj.data);
         }
+    });
+
+    document.getElementById("sortAscByCost").addEventListener("click", function(){
+        sortAndShowProducts(ORDER_ASC_BY_COST);
+    });
+
+    document.getElementById("sortDescByCost").addEventListener("click", function(){
+        sortAndShowProducts(ORDER_DESC_BY_COST);
+    });
+
+    document.getElementById("sortBySoldCount").addEventListener("click", function(){
+        sortAndShowProducts(ORDER_BY_SOLD_COUNT);
     });
 });
